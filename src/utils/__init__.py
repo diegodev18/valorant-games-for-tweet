cc9caeb4a0fd44341ddb94eb42231beaccb2d587
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-07-22 06:00:54 -0600

    Rename send_noti_telegrambot.py to send_telegram.py in utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,6 +2,6 @@ from .get_times import get_sleep_time
 from .get_twitter_api import x_auth
 from .make_tweet import make_tweet_main
 from .send_getcode_exportcode import get_code
-from .send_noti_telegrambot import send_message
+from .send_telegram import send_message
 
 __all__ = ["get_sleep_time", "x_auth", "make_tweet_main", "get_code", "send_message"]

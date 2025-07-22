ecd9c630bf4cf40efd0d8acc2402c2b005169910
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-07-22 05:33:43 -0600

    Refactor imports to use src.utils module

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from datetime import datetime, timedelta
 from os import system
 from platform import platform
 from time import sleep
-
-from get_times import get_sleep_time
-from get_twitter_api import get_api_main
-from make_tweet import make_tweet_main
-from send_getcode_exportcode import get_code
-from send_noti_telegrambot import send_telegram_main
+from src.utils import (
+    get_sleep_time,
+    get_api_main,
+    make_tweet_main,
+    get_code,
+    send_telegram_main
+)
 
 telegram_codes = get_code()  # {'bot_token': bot_token, 'chat_id': chat_id}
 

8be02f57a4b0337a23a65a4d953c80f70383ea23
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-07-22 06:59:39 -0600

    Update get_env import and remove required arg from calls

--- src/utils/keys.py
+++ src/utils/keys.py
@@ -1,9 +1,9 @@
-from get_env import get as get_env
+from get_env import get_env
 
 
 def get() -> tuple:
-    TWITTER_KEY = get_env("TWITTER_KEY", True)
-    TWITTER_SECRET = get_env("TWITTER_SECRET", True)
+    TWITTER_KEY = get_env("TWITTER_KEY")
+    TWITTER_SECRET = get_env("TWITTER_SECRET")
     TWITTER_ACCESS_TOKEN = get_env("TWITTER_ACCESS_TOKEN")
     TWITTER_ACCESS_TOKEN_SECRET = get_env("TWITTER_ACCESS_TOKEN_SECRET")
 

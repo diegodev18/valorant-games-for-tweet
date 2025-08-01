164e00f4139e71e777120b6681a3feb6d6eb5dfe
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-01 01:08:20 -0600

    Add get_multi_env to get_env file

--- src/utils/get_env.py
+++ src/utils/get_env.py
@@ -4,8 +4,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_env(key) -> str:
+def get_env(key: str) -> str | None:
     env_value = environ.get(key)
     if env_value:
         return env_value
-    raise KeyError(f"Key \'{key}\' not found in environment variables")
+    return None
+
+
+def get_multi_env(*keys):
+    return (get_env(key) for key in keys)
+
+
+if __name__ == "__main__":
+    LIST, DATA = get_multi_env("LIST", "DATA")
+    print(LIST, DATA)
+

8685e7bd87f2543dca79f365e7fdee742a970c5d
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-14 18:14:30 -0600

    Refactor app.py to include environment variable handling and adjust tweet timing logic based on production environment

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from datetime import datetime, timedelta
 from os import system
 from platform import platform
 from time import sleep
-from src.utils import get_sleep_time, x_auth, make_tweet_main, send_message
+from sys import exit
+from src.utils import get_sleep_time, x_auth, make_tweet_main, send_message, get_env
+
+env_production = get_env("PYTHON_ENV") == "production"
 
 
 def main():
@@ -10,7 +13,7 @@ def main():
     send_message('SCRIPT "VALORANG_GAMES_FOR_TWEET" INICIADO CON EXITO!')
     system("cls" if "windows" in platform().lower() else "clear")
     while True:
-        games_time = get_sleep_time(day=datetime.now().day + 1, hour=8)
+        games_time = get_sleep_time(day=datetime.now().day + 1, hour=8) if env_production else 10
         msg = (
             f"Esperando tiempo para CREAR el tweet... Se CREARA el {datetime.now() + timedelta(0, games_time)}!\n"
             if games_time
@@ -23,7 +26,7 @@ def main():
         tweet = make_tweet_main(datetime.now().day)
         print(f"TWEET CREADO\n{tweet}\n")
 
-        tweet_time = get_sleep_time(day=datetime.now().day, hour=18)
+        tweet_time = get_sleep_time(day=datetime.now().day, hour=18) if env_production else 10
         msg = (
             f"\nEsperando tiempo para SUBIR el tweet... Se SUBIRA el {datetime.now() + timedelta(0, tweet_time)}!\n"
             if tweet
@@ -54,15 +57,18 @@ def main():
 
 
 if __name__ == "__main__":
-    while True:
-        try:
-            main()
-        except KeyboardInterrupt:
-            print("\nAdios!")
-            send_message('SCRIPT "VALORANG_GAMES_FOR_TWEET" FINALIZADO CON EXITO!')
-            break
-        except Exception as error:
-            error_msg = error.with_traceback(None)
-            print(f"Ah ocurrido un error en el script!\n{error_msg}")
-            send_message(f"Ah ocurrido un error en el script!\n\nERROR:\n{error_msg}")
-            sleep(24 * 60 * 60)
+    if env_production:
+        while True:
+            try:
+                main()
+            except KeyboardInterrupt:
+                print("\nAdios!")
+                send_message('SCRIPT "VALORANG_GAMES_FOR_TWEET" FINALIZADO CON EXITO!')
+                break
+            except Exception as error:
+                error_msg = error.with_traceback(None)
+                print(f"Ah ocurrido un error en el script!\n{error_msg}")
+                send_message(f"Ah ocurrido un error en el script!\n\nERROR:\n{error_msg}")
+                sleep(24 * 60 * 60)
+    else:
+        main()
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,5 +2,6 @@ from .get_times import get_sleep_time
 from .x_auth import x_auth
 from .make_tweet import make_tweet_main
 from .send_telegram import send_message
+from .get_env import get_env
 
-__all__ = ["get_sleep_time", "x_auth", "make_tweet_main", "send_message"]
+__all__ = ["get_sleep_time", "x_auth", "make_tweet_main", "send_message", "get_env"]

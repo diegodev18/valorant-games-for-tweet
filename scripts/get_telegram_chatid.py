17fc5978f27bd6c1ad3ae6bb28a6c00bdf3fa6f5
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-09-01 21:04:32 -0600

    Refactor get_chatid function to improve readability and update token retrieval method

--- scripts/get_telegram_chatid.py
+++ scripts/get_telegram_chatid.py
@@ -1,21 +1,45 @@
-from src.utils.get_env import get_env
+from dotenv import load_dotenv
+from os import getenv
 from requests import get
 
+load_dotenv()
 
-def get_chatid(bot_token: str, name=None):
-    url = f"https://api.telegram.org/bot{bot_token}/getUpdates"
+
+def get_updates_url(BOT_TOKEN: str | None):
+    if not BOT_TOKEN:
+        return None
+    return f"https://api.telegram.org/bot{BOT_TOKEN}/getUpdates"
+
+
+def get_chatid(BOT_TOKEN: str | None, first_name=None):
+    url = get_updates_url(BOT_TOKEN)
+    if not url:
+        print("No se ha proporcionado el token del bot")
+        return None
+
     response = get(url)
+    data = response.json()
+
+    print("Datos recibidos:", data, sep="\n")
+
     if response.status_code == 200:
-        data = response.json()
         if len(data["result"]) > 0:
-            if name:
+            if first_name:
+                lower_name = first_name.lower()
                 for i in data["result"]:
-                    if i["message"]["from"]["first_name"].lower() == name:
-                        return i["message"]["chat"]["id"]
+                    if i["message"]["from"]["first_name"].lower() == lower_name:
+                        chat_id = i["message"]["chat"]["id"]
+                        print(f"Chat ID de {first_name}: {chat_id}")
+                        return chat_id
             else:
-                return data["result"][-1]["message"]["chat"]["id"]
+                chat_id = data["result"][-1]["message"]["chat"]["id"]
+                print(f"Chat ID del último usuario: {chat_id}")
+                return chat_id
+
+    print("No se pudo obtener el chat ID")
+    return None
 
 
 if __name__ == "__main__":
-    bot_token: str = get_env("TELEGRAM_BOT_TOKEN")
-    print(get_chatid(bot_token))
+    BOT_TOKEN: str | None = getenv("TELEGRAM_BOT_TOKEN")
+    get_chatid(BOT_TOKEN, "Diego")

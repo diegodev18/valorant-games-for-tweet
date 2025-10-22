00def4189f17cbd8c56726a01b120119b6d86ab1
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-22 07:04:19 -0600

    Refactor generate_text function to include system instructions and correct content structure for improved text generation

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -1,10 +1,26 @@
 from lib import LLMClient
+from google.genai import types
 
 def generate_text(prompt: str) -> str:
     try:
+        systemInstructions = [
+            types.Content(
+                role="system",
+                parts=[types.Part(text="""\
+You are an Agent for automated text generation. Make a message about Valorant Games based on the user's prompt.\
+    """)],
+            )
+        ]
+        contents = [
+            types.Content(
+                role="user",
+                parts=[types.Part(text=prompt)]
+            )
+        ]
         response = LLMClient.models.generate_content(
             model="gemini-2.5-flash",
-            contents=prompt,
+            contents=contents,
+            systemInstructions=systemInstructions,
         )
         return response.text
     except Exception as e:
